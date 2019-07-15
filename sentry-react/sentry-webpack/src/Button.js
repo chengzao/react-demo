b9a95198ecadbda3eda7386509382183a5beb14f
chengzao <czhlink@163.com>
2019-07-15 17:33:56 +0800

    Add Sentry captureMessage

--- sentry-react/sentry-webpack/src/Button.js
+++ sentry-react/sentry-webpack/src/Button.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-
+import * as Sentry from '@sentry/browser'
 class Button extends Component{
   constructor(){
     super()
@@ -9,10 +9,17 @@ class Button extends Component{
     this.methodDoesNotExist = this.methodDoesNotExist.bind(this)
   }
   methodDoesNotExist(){
-    throw new Error(`我是一个抛出的错误日志: ${new Date()}`)
+    // throw new Error(`我是一个抛出的错误日志: ${new Date()}`)
+    try {
+      console.log(a);
+      let a = 2
+    } catch (error) {
+      // 主动触发捕获错误
+      Sentry.captureMessage(error);
+    }
   }
   render(){
-    return (<button onClick = { this.methodDoesNotExist } > Break the world</button>) 
+    return (<button onClick = { this.methodDoesNotExist } > Break the world</button>)
   }
 }
 
